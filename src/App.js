0841feb5ded5ec40d9e3a7969737715819501ae5
Amelia <amelia.holds@yahoo.com.au>
2024-06-24 15:31:32 +0930

    add error boundary testing and code for deployment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from 'react';
 import Main from './components/MainComponent'
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
+import ErrorBoundary from "./components/Error Boundaries/ErrorBoundaries.js";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Main/>
+      <React.StrictMode>
+        <ErrorBoundary>
+          <Main/>
+        </ErrorBoundary>
+      </React.StrictMode>
       </BrowserRouter>
     </div>
   );
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -11,15 +11,15 @@ function Header (){
             <nav className="navlist" style={{display:"inline-block",width:"550px",alignSelf:"flex-end"}}>
                 <ul style={{display: "inline-flex"}}>
                     <li style={{listStyle:"none",display:"inline",padding:"0 20px"}}>
-                    <Link to='/fixtures'><a className="navButtonFix">Fixture</a></Link></li>
+                    <Link to='/fixtures'><a className="navButtonFix" style={{color:"white",textDecoration:"none"}}>Fixture</a></Link></li>
                     <li style={{listStyle:"none",display:"inline",padding: "0 20px"}}>
-                    <Link to='/stats'><a className="navButtonStats">Stats</a></Link></li>
+                    <Link to='/stats'><a className="navButtonStats" style={{color:"white",textDecoration:"none"}}>Stats</a></Link></li>
                     <li style={{listStyle:"none",display:"inline",padding: "0 20px"}}>
-                    <Link to='/interviews'><a className="navButtonInt">Interviews</a></Link></li>
+                    <Link to='/interviews'><a className="navButtonInt" style={{color:"white",textDecoration:"none"}}>Interviews</a></Link></li>
                     <li style={{listStyle:"none",display:"inline",padding: "0 20px"}}>
-                    <Link to='/join'><a className="navButtonJoin">Join us</a></Link></li>
+                    <Link to='/join'><a className="navButtonJoin" style={{color:"white",textDecoration:"none"}}>Join us</a></Link></li>
                     <li style={{listStyle:"none",display:"inline",padding: "0 20px"}}>
-                    <Link to='/contact'><a className="navButtonContact">Contact</a></Link></li>
+                    <Link to='/contact'><a className="navButtonContact" style={{color:"white",textDecoration:"none"}}>Contact</a></Link></li>
                 </ul>
             </nav>
         
